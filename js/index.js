ba0f8379195a0aa3d147803223417c63acd4edb9
Eddie Lin <elin@mozilla.com>
2015-12-06 07:09:24 +0800

    Separate popup relevant code, minor RWD fix.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 $(function() {
-  var $popup = $('#popup');
-
   $('#banners').flickity({
     pageDots: false,
     wrapAround: true,
@@ -42,49 +40,5 @@ $(function() {
       });
       $(this).masonry('appended', $newItems);
     });
-  });
-
-  $('input[name=tabs]').on('change', function() {
-    initScreenshots();
-    initRelated();
-  });
-
-  function initScreenshots() {
-    var $screenshots = $('#screenshots');
-    var flkt = $screenshots.data('flickity');
-    if (!flkt && $screenshots.is(':visible')) {
-      console.log('initScreenshots');
-      $screenshots.flickity({
-        contain: true
-      });
-    }
-  }
-
-  function initRelated() {
-    var $related = $('#related');
-    var flkt = $related.data('flickity');
-    if (!flkt && $related.is(':visible')) {
-      console.log('initRelated');
-      $related.flickity({
-        contain: true
-      });
-    }
-  }
-
-  function showPopup() {
-    $popup.removeClass('hide');
-    initScreenshots();
-    initRelated();
-  }
-
-  function hidePopup() {
-    $popup.addClass('hide');
-  }
-
-  $('#close-popup').click(hidePopup);
-  $popup.click(function(e) {
-    if (e.target == this) {
-      hidePopup();
-    }
   });
 });
